fb9316d2074f210845b2a1e26c47cb8c23ee9b02
Aniruddha Sherkar <sherkar.aniruddha@gmail.com>
2012-08-23 18:33:06 +0530

    First commit without extension

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Spreestore::Application.config.secret_token = 'd97e3629872e6ffde3a47cefab313c5085d8c0d1bce84441fa942eb82033d0172f7067ac855a3f5c41242accc5a906b052933bc449c9d58b4ee413fee720f5d6'
